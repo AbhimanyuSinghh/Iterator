f35cb0c450d84e98f43ae45b818500c55f03db0e
Abhimanyu Singh <abhimanyu22nits@gmail.com>
2023-09-27 00:00:58 +0530

    my range function

---
+++ myRangeFunction.py
@@ -0,0 +1,35 @@
+'''
+a program, having
+
+'''
+
+class myRange:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+    
+    def __iter__(self):
+        return myRangeIterator(self)
+    
+
+class myRangeIterator:
+    def __init__(self, iterable_obj):
+        self.iterable = iterable_obj
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.iterable.start >= self.iterable.end:
+            raise StopIteration
+        current = self.iterable.start
+        self.iterable.start +=1
+        return current
+
+
+x = myRange(1,11)
+for i in x:
+    print(i)
+        
+print(type(x))
+    
